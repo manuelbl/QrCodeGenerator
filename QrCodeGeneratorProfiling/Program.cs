e2d2cebdbb3cb41cc79480a9239392a22c88525b
manuelbl <manuel.bleichenbacher@gmail.com>
2019-02-10 20:16:50 +0100

    Add performance profiling program

---
+++ QrCodeGeneratorProfiling/Program.cs
@@ -0,0 +1,68 @@
+﻿/* 
+ * QR code generator library (.NET)
+ *
+ * Copyright (c) Manuel Bleichenbacher (MIT License)
+ * https://github.com/manuelbl/QrCodeGenerator
+ * Copyright (c) Project Nayuki (MIT License)
+ * https://www.nayuki.io/page/qr-code-generator-library
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+using System;
+using System.Diagnostics;
+using static Net.Codecrete.QrCodeGenerator.QrCode;
+
+namespace Net.Codecrete.QrCodeGenerator.Profiling
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Stopwatch watch = Stopwatch.StartNew();
+            Ecc[] eccLevels = { Ecc.Low, Ecc.Medium, Ecc.Quartile, Ecc.High };
+
+            int textLength = SampleText.Length;
+            for (int len = 5; len <= textLength; len++)
+            {
+                foreach (Ecc ecc in eccLevels)
+                {
+                    EncodeText(SampleText.Substring(0, len), ecc);
+                }
+            }
+
+            watch.Stop();
+            long elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine("Elapsed time: {0}", elapsedMs / 1000.0);
+        }
+
+        private const string SampleText = @"Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
+accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto 
+beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed 
+quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem 
+ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et 
+dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis 
+suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea 
+voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? 
+At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque 
+corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa 
+qui officia deserunt mollitia animi, id est laborum et dolorum fuga.";
+    }
+}
